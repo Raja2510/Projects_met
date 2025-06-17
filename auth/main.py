9335edf5f9b71710cc762cb46179a07ad3d54aad
Raja251 <rajasimha.2510@gamil.com>
2025-06-17 15:58:53 +0530

    17-06

--- auth/main.py
+++ auth/main.py
@@ -1,32 +1,42 @@
+from supabase import create_client
 from fastapi import FastAPI
 
 app = FastAPI()
 
-library = []
+supabase_url = "https://bzxbqtofhsydqxxnythj.supabase.co"
+supabase_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6eGJxdG9maHN5ZHF4eG55dGhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4MjE5OTUsImV4cCI6MjA2NTM5Nzk5NX0.DdrA-hhjFYyAZBSlPAToK_CC_1o7EKPzOIpl_murS6E"
 
-@app.post("/book/add")
-def add_book(name, desc):
-    library.append({"name": name, "desc": desc})
-    return library
+database = create_client(supabase_url, supabase_api_key)
 
-@app.put("/book/update")
-def update_book(name, updated_desc):
-    for book in library:
-        if book['name'] == name:
-            book["desc"] = updated_desc
-            return library
+#Read
+# result = database.table("app_users").select('*').execute()
+
+@app.get("/users")
+def read_users():
+    result = database.table("app_users").select('*').execute()
+    return result.data
+
+@app.post('/create')
+def create_user(name,username, age, password):
+    value=database.table("app_users").select("*").eq("username",username).eq("password",password).execute()
+    if len(value.data)==0:  
+        result = database.table('app_users').insert({
+            'name': name,
+            'age': age,
+            'username':username,
+            'password': password
+        }).execute()
+
+        return "registerd"
     else:
-        return "No book found"
+        return "this credentials already exsist"
 
 
-@app.delete("/book/delete")
-def delete_book():
-    return "Hello"
-
-@app.get("/book/get")
-def get_book():
-    return "Hello"
-
-
-
-#Adding somethign else
+@app.post('/login')
+def login(username,password):
+    value=database.table("app_users").select("*").eq("username",username).eq("password",password).execute()
+    if len(value.data)==0:
+        return False
+    else :
+        return True
+    
