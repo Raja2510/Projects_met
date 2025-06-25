d43bb1741ca25265d5aec7d03762c43bb1f203cb
Raja251 <rajasimha.2510@gamil.com>
2025-06-25 21:15:05 +0530

    new commit

--- inv/main.py
+++ inv/main.py
@@ -8,7 +8,7 @@ database=create_client(supabase_url=database_url,supabase_key=database_api)
 
 app =FastAPI()
 
-# --------------supplier-----------------------------
+# --------------supplier---------------------------------------------
 
 @app.post("/supplier")
 def get_supplier():
@@ -32,22 +32,53 @@ def create_supplier(name,age,contact):
         return "This person already exsist"
 
 @app.put("/supplier/update")
-def update_supplier(id,name,age,contact):
-    get_supplier=database.table("supplier").select("*").eq("id",id).execute()
+def update_supplier(name,uname,age,contact):
+    get_supplier=database.table("supplier").select("*").eq("supplier_name",name).execute()
     if get_supplier.data==[]:
         return  "This ID Doesnt Exsists"
     else:
         updated_supplier=database.table("supplier").update(
-            {"supplier_name":name,
+            {"supplier_name":uname,
              "supplier_age":age,
              "contact_info":contact
              }
-        ).eq("id",id).execute()
+        ).eq("supplier_name",name).execute()
         return updated_supplier.data
 @app.delete("/supplier")
-def update_supplier(name):
+def delete_supplier(name):
     del_supplier=database.table("supplier").delete().eq("supplier_name",name).execute()
     if del_supplier.data==[]:
         return  "This ID Doesnt Exsists"
     else:
         return del_supplier.data
+    
+
+
+
+
+
+
+
+
+#------------------categories---------------------
+
+
+@app.get("/category")
+def get_supplier():
+    result = database.table("category").select('*').execute()
+    return result
+
+@app.post("/category/create")
+def create_supplier(name,desc):
+    valid=database.table("category").select('*').eq("category_name",name).execute()
+    if len(valid.data)==0:
+        new_entry=database.table('category').insert(
+            {
+                'category_name':name,
+                'descreption':desc
+                
+            }
+        ).execute()
+        return new_entry.data
+    else:
+        return "This person already exsist"
